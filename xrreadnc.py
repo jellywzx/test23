c0b1d3bc1a4f7a33e88319d1f9162d2b431c3553
fzjxwzx <fzjxwzx@126.com>
2021-10-16 22:49:53 +0800

    Initial commit

--- xrreadnc.py
+++ xrreadnc.py
@@ -31,7 +31,7 @@ swh_KU = cryosat2['SWH_KU']
 # jasonswhKU.plot(color ='b')
 
 
-#20211011
+#对nc格式数据进行重采样
 cryosat2 = xr.open_dataset('D:/wzx/data/CRYOSAT-2/IMOS_SRS-Surface-Waves_MW_CRYOSAT-2_FV02_000N-000E-DM00.nc')
 swh_KU = cryosat2['SWH_KU']
 swh_KUdataframe=swh_KU.to_dataframe()
@@ -43,12 +43,44 @@ swh_KU.resample(TIME='SM').mean()
 swh_KU.groupby('TIME.dayofyear')
 swh_KU.groupby('TIME.year')
 
-month_length=swh_KU.TIME.dt.days_in_month
-a=swh_KU.sel(TIME='2010-8').to_series()
+month_length = swh_KU.TIME.dt.days_in_month
+a = swh_KU.sel(TIME='2010-8').to_series()
 
-filePath='D:/wzx/data/test'
-for dirpath, dirnames, filenames in os.walk(filePath):
-    data = xr.open_dataset(filenames)
-for dirpath, dirnames, filenames in os.walk(filePath):
-        data = xr.open_dataset(filenames)
+# 读取多个nc文件
+filePath = 'D:/wzx/data/test'
+files = os.listdir(filePath)
+# for file in files:
+#     f = xr.open_dataset(filePath+"\\"+file)
+#     swh_KU=f['SWH_KU']
+
+# for dirpath, dirnames, filenames in os.walk(filePath):
+#     data = xr.open_dataset(filenames)
+#     print(data)
+
+for file in files:
+    f=xr.open_dataset(filePath+"\\"+file)
+    swh_KU = f['SWH_KU']
+    lon = f['LONGITUDE']
+    lat = f['LATITUDE']
+    time = f['TIME']
+    swh_KUdataframe = swh_KU.to_dataframe()
+    swh_resampled = swh_KUdataframe.groupby([(swh_KUdataframe.index.year), (swh_KUdataframe.index.month)]).mean()
+    # swh_resampled=swh_resampled.to_dataarray()
+    ds=xr.Dataset(
+        data_vars={'swh_KU':(['TIME'], swh_resampled['SWH_KU'])},
+        # dict(swh_KU=(['TIME'],swh_resampled)),
+        coords=dict(lon=(["TIME"], swh_resampled['LONGITUDE']),
+                    lat=(['TIME'], swh_resampled['LATITUDE']),
+                    TIME=pd.to_datetime(swh_resampled.index.get_level_values(0).astype(str)+'-'+swh_resampled.index.get_level_values(1).astype(str),format='%Y-%m'))
+
+    )
+    # d = xr.DataArray(
+    #     data=swh_resampled['SWH_KU'],
+    #     dims=['TIME'],
+    #     coords=dict(
+    #         lon=("LONGITUDE", swh_resampled['LONGITUDE']),
+    #         lat=("LATITUDE", swh_resampled['LATITUDE']),
+    #         time=time
+    #     )
+    # )
 
